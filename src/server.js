24f0af3a4b7301718f8bc01a17d64c6588a8def4
Michael Prather <mprather1@gmail.com>
2017-03-29 04:53:18 +0000

    1.1.0

--- src/server.js
+++ src/server.js
@@ -1,8 +1,6 @@
 import {Server} from 'http'
 
-export default function getHTTPServer (props) {
-  const {app, port, environment} = props
-  
+export default function getHTTPServer (app, port, environment) {
   const server = Server(app).listen(port, function () {
     if (environment === 'development') {
       console.log('Listening on port ' + port + '...')
