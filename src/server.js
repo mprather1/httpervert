96236a2b036df639cebacc346de88b1834559f8e
Michael Prather <mprather1@gmail.com>
2017-03-30 04:42:21 +0000

    1.1.3

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import {Server} from 'http'
+import chalk from 'chalk'
 
 export default function getHTTPServer (options) {
   const {app, port, environment, logger} = options
@@ -6,6 +7,13 @@ export default function getHTTPServer (options) {
   const server = Server(app).listen(port, function () {
     if (environment === 'development') {
       logger.info('Listening on port', port + '...')
+    }
+  })
+  server.timeout = 12000
+  
+  server.on('request', function (req, res) {
+    if (environment === 'development') {
+      logger.info(chalk.cyan("processing..."))
     }
   })
   return server
