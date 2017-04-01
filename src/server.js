40fb6aa1487d138ed626796ab50f92a3b12f9f12
Michael Prather <mprather1@gmail.com>
2017-03-31 19:22:54 -0500

    1.1.5

--- src/server.js
+++ src/server.js
@@ -5,9 +5,8 @@ export default function getHTTPServer (options) {
   const {app, port, environment, logger} = options
   
   const server = Server(app).listen(port, function () {
-    if (environment === 'development') {
-      logger.info('Listening on port', port + '...')
-    }
+    logger.info('Environment:', environment)
+    logger.info('Listening on port', port + '...')
   })
   server.timeout = 12000
   
