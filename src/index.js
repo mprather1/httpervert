ef09145c650e06b3c159f1e1b7ead41ee5e5f8f5
Michael Prather <mprather1@gmail.com>
2017-05-26 02:10:16 +0000

    https

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
-import {Server} from 'http'
+import http from 'https'
+import fs from 'fs'
 import chalk from 'chalk'
 
 export default function getHTTPServer (options) {
-  const {app, port, logger, packageName} = options
+  const {app, port, logger, packageName, config} = options
 
-  const server = Server(app).listen(port, function () {
+  const sslPath = '/etc/letsencrypt/live/' + config.domainName + '/'
+
+  var serverConfig = {
+    key: fs.readFileSync(sslPath + 'privkey.pem'),
+    cert: fs.readFileSync(sslPath + 'fullchain.pem')
+  }
+
+  const server = http.createServer(serverConfig, app)
+
+  server.on('listening', function () {
     logger.info(`${chalk.bgBlack.cyan(packageName)} listening on port ${chalk.green(port)}...`)
   })
+
   server.timeout = 12000
+  server.listen(port)
 
   process.on('SIGINT', function () {
     console.log('\nGoodbye! Thanks for coming...')
