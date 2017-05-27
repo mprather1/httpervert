41b00c6bdd2c973bf5a9e0c251e283de857621c0
Michael Prather <mprather1@gmail.com>
2017-05-27 23:49:18 +0000

    ssl

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import http from 'https'
 import fs from 'fs'
 import chalk from 'chalk'
 
 export default function getHTTPServer (options) {
-  const {app, port, environment, logger, packageName, config} = options
+  const {app, environment, logger, packageName, config} = options
 
   const sslPath = config.sslPath + config.domainName[environment] + '/'
 
@@ -12,19 +11,27 @@ export default function getHTTPServer (options) {
     cert: fs.readFileSync(sslPath + 'fullchain.pem')
   }
 
-  const server = http.createServer(serverConfig, app)
+  const http = require('http').Server(app)
+  const https = require('https').Server(serverConfig, app)
 
-  server.on('listening', function () {
+  const handleListening = function (port) {
     logger.info(`${chalk.bgBlack.cyan(packageName)} listening on port ${chalk.green(port)}...`)
+  }
+  
+  http.addListener('listening', function () {
+    handleListening(80)
+  })
+  
+  https.addListener('listening', function () {
+    handleListening(8000)
   })
 
-  server.timeout = 12000
-  server.listen(port)
-
+  http.listen(8000)
+  https.listen(443)
+  
   process.on('SIGINT', function () {
     console.log('\nGoodbye! Thanks for coming...')
     process.exit()
   })
 
-  return server
 }
