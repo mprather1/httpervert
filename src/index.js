174e3a6036ac2cf11c5746f35cd557ab3d2a5f49
Michael Prather <mprather1@gmail.com>
2017-05-30 18:45:44 +0000

    removed https

--- src/index.js
+++ src/index.js
@@ -1,35 +1,20 @@
-import fs from 'fs'
 import chalk from 'chalk'
 
 export default function getHTTPServer (options) {
-  const {app, environment, port, logger, packageName, config} = options
+  const { app, port, logger, packageName } = options
 
-  var http
-  
-  if (environment === 'production') {
-    const sslPath = config.sslPath + config.domainName[environment] + '/'
-  
-    var serverConfig = {
-      key: fs.readFileSync(sslPath + 'privkey.pem'),
-      cert: fs.readFileSync(sslPath + 'fullchain.pem')
-    }    
-  
-    http = require('https').Server(serverConfig, app)
-  } else {
-    http = require('http').Server(app)
-  }
-  
+  var http = require('http').Server(app)
+
   const handleListen = function () {
     logger.info(`${chalk.bgBlack.cyan(packageName)} listening on port ${chalk.green(port)}...`)
   }
-  
-  http.addListener('listening', handleListen)
+
+  http.on('listening', handleListen)
 
   http.listen(port)
-  
+
   process.on('SIGINT', function () {
     console.log('\nGoodbye! Thanks for coming...')
     process.exit()
   })
-
 }
