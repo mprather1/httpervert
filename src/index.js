948effc0e4be26da634ba43b93d305d64df3440b
Michael Prather <mprather1@gmail.com>
2017-05-30 21:51:56 +0000

    display version

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import chalk from 'chalk'
 
 export default function getHTTPServer (options) {
-  const { app, port, logger, packageName } = options
+  const { app, port, logger, packageName, packageVersion } = options
 
   var http = require('http').Server(app)
 
   const handleListen = function () {
-    logger.info(`${chalk.bgBlack.cyan(packageName)} listening on port ${chalk.green(port)}...`)
+    logger.info(`${chalk.bgBlack.cyan(packageName)} ver.${chalk.bgBlack.green(packageVersion)} listening on port ${chalk.green(port)}...`)
   }
 
   http.on('listening', handleListen)
