e4ea66d75a707f66b1f784b327472ca8f259c010
Michael Prather <mprather1@gmail.com>
2017-07-29 16:45:18 +0000

    remove shintech-info and add req.statusCode

--- src/index.js
+++ src/index.js
@@ -1,28 +1,31 @@
 import chalk from 'chalk'
-import {getAppInfo as info} from 'shintech-info'
+import http from 'http'
 
 export default function getHTTPServer (options) {
-  const { app, port, logger } = options
+  const { app, port, logger, packageDir } = options
 
-  var http = require('http').Server(app)
+  const server = http.Server(app)
 
-  function handleListen () {
-    info(options)
+  const packageName = packageDir.name
+  const packageVersion = packageDir.version
+
+  function handleListening () {
+    logger.info(`${chalk.bgBlack.cyan(packageName)} ver.${chalk.bgBlack.green(packageVersion)} listening on port ${chalk.bgBlack.yellow(port)}`)
   }
 
   function handleRequest (req, res, next) {
-    logger.info(`${chalk.yellow(req.method)} => ${req.url}`)
+    logger.info(`${chalk.yellow(res.statusCode)} - ${chalk.green(req.method)} => ${req.url}`)
   }
 
-  http.on('listening', handleListen)
+  server.on('listening', handleListening)
 
-  http.on('request', handleRequest)
-  http.listen(port)
+  server.on('request', handleRequest)
+  server.listen(port)
 
   process.on('SIGINT', function () {
     console.log('\nGoodbye! Thanks for coming...')
     process.exit()
   })
 
-  return http
+  return server
 }
